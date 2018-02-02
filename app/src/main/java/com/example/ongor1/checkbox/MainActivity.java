af04adbd5b27a287eac5f980791f50339bf52ab4
kanjana <kanjanapinjantha.kate@gmail.com>
2018-02-02 10:49:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ongor1/checkbox/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ongor1.checkbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CheckBox momCheckBox;
+    private CheckBox dadCheckBox;
+    private CheckBox grandpaCheckBox;
+    private Button showButton;
+    private TextView showTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        momCheckBox = (CheckBox)findViewById(R.id.MomCheckBoxID);
+        dadCheckBox = (CheckBox)findViewById(R.id.DadID);
+        grandpaCheckBox = (CheckBox)findViewById(R.id.grandpaID);
+        showTextView = (TextView)findViewById(R.id.resultID);
+        showButton = (Button)findViewById(R.id.showButtonID);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder
+                        .append(momCheckBox.getText().toString()+"satus is:"+
+                        momCheckBox.isChecked()+ "\n");
+                stringBuilder
+                        .append(momCheckBox.getText().toString()+"satus is:"+
+                        dadCheckBox.isChecked() +"\n");
+                stringBuilder
+                        .append(momCheckBox.getText().toString()+"satus is:"+
+                        grandpaCheckBox.isChecked()+ "\n");
+
+                showTextView.setText(stringBuilder);
+            }
+        });
+
+    }
+}
